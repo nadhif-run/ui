8b33e6bed486f78a940300e194668ea5b3101288
nadhif-run <nadhifthoriqiii@gmail.com>
2026-05-20 14:03:03 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 async function getData() {
     try {
-        const response = await fetch('https://semioratorical-unbreakably-dacia.ngrok-free.dev/');
+        const response = await fetch('http://192.168.1.4/thorix/');
         
         if (!response.ok) {
             throw new Error("Sistem sedang dalam pemeliharaan");
         }
 
         // window.location.href = "https://semioratorical-unbreakably-dacia.ngrok-free.dev/";
-        window.location.replace("https://semioratorical-unbreakably-dacia.ngrok-free.dev/");
+        window.location.replace("http://192.168.1.4/storage/");
 
     } catch (error) {
         console.error('Error:', error);
